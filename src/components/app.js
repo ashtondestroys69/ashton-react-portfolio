9e03e9e29cb5bebe6033e8e4de1dc350b36153eb
Ashton Washburn <ashton.uvu@gmail.com>
2020-12-07 02:32:27 -0700

    Updated List

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import moment from "moment"; 
 
+
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Ashton Washburn's Portfolio</h1>
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
         <h2>An Introduction to Introspection</h2>
         <h3>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h3>
       </div>
