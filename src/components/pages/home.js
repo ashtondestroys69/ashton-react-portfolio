5b75dbd1383fc7a1c19c67c1b2c1b8adf9694168
Ashton Washburn <ashton.uvu@gmail.com>
2021-05-20 05:23:29 -0600

    Portfolio Manager Update

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -6,28 +6,15 @@ import PortfolioContainer from "../portfolio/portfolio-container";
 export default function() {
     return (
         <div>
-            <div className="hell" hidden>
-                <h2>Welcome to Hell; may you burn for eternity.</h2>
-
-                {/* Questions:
-                    - How to get gif or mp4 working
-                    - how to get bottom border closer to text
-                    - how to shorten length of bottom border */}
-
-                {/* <video className="hell-vid" autoPlay loop muted>
-                    <source src={`${Flames}`} type="video/mp4"></source>
-                </video> */}
-
-                {/* <video width="666px" height="45px" autoPlay muted loop>
-                    <source src="..\images\gif\flameshell.mp4" type="video/mp4"></source>
-                </video> */}
-
-                 {/* <div className="hellgif">
-                    <img src="../images/gif/burnhell.gif" alt="Burn in Hell"></img>
-                </div> */}
+            <div className="hellContainer">
+                <div className="hell">
+                    <h2>Welcome to Hell; may you burn for eternity.</h2>
+                </div>
             </div>
-            
-            <PortfolioContainer/>
+
+            <div className="spacer34" />
+
+            <PortfolioContainer />
         </div>
     );
 }
